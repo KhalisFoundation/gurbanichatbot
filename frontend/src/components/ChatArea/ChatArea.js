a07db0a86a58dbb28228a626b3dffebb29f0abaf
Tashneet Kaur <tashneetkaur343@gmail.com>
2024-10-11 01:53:49 +0530

    expand icon working

--- frontend/src/components/ChatArea/ChatArea.js
+++ frontend/src/components/ChatArea/ChatArea.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import '../ChatScreenPage/ChatScreenPage.css';
-
+import expandicon from '../../assets/images/Frame.png'; 
 import image from '../../assets/images/response.png'; 
 import blueCircleImage from '../../assets/images/youlogo.png'; // Blue circle image
 import './ChatArea.css';
+import ExpandedShabad from './ExpandedShabad'; // Import the expanded component
 
 const ChatArea = ({ query, setQuery, handleQuerySubmit, loading, shabadDetails, selectedSource }) => {
-  // Sample suggestions for Gurbani questions
   const suggestions = [
     "When to pray?",
     "Who created Vedas?",
@@ -14,59 +14,52 @@ const ChatArea = ({ query, setQuery, handleQuerySubmit, loading, shabadDetails, 
     "How to attain salvation?",
   ];
 
-  // State to track if the user has started typing
   const [showSuggestions, setShowSuggestions] = useState(false);
-  
-  // State to track the submitted query
   const [submittedQuery, setSubmittedQuery] = useState('');
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
-    // Show suggestions only if the input is not empty
     setShowSuggestions(e.target.value.trim() !== '');
   };
 
-  // Update the submit handler to set the submitted query
   const handleFormSubmit = (e) => {
     e.preventDefault();
     if (query.trim()) {
-      setSubmittedQuery(query); // Set the submitted query
-      handleQuerySubmit(e);     // Call the original submit handler
+      setSubmittedQuery(query);
+      handleQuerySubmit(e);
     }
   };
 
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="chat-area">
-      {/* Display the Question at the Top */}
-      {submittedQuery && (
+    <div className={`chat-area ${isExpanded ? 'expanded' : ''}`}>
+      {!isExpanded && submittedQuery && (
         <div className="question-display-top">
           <span className="question-top-text">{submittedQuery}</span>
         </div>
       )}
 
-      {/* Horizontal Line Separator */}
-      <hr className="question-separator" />
+      {!isExpanded && <hr className="question-separator" />}
 
-      {/* You Section with the Question */}
-      {submittedQuery && (
+      {!isExpanded && submittedQuery && (
         <div className="question-source-section">
-          {/* Message bubble */}
           <div className="question-display">
             <div className="circle blue-circle">
               <img src={blueCircleImage} alt="Blue Circle" className="logo-small" />
             </div>
-            {/* Sender label "You" in the same line as the blue circle */}
             <span className="sender-label">You</span>
           </div>
-          {/* The question text moves below "You" */}
           <div className="question-text-wrapper">
             <span className="question-text">{submittedQuery}</span>
           </div>
         </div>
       )}
 
-      {/* Display Source */}
-      {submittedQuery && (
+      {!isExpanded && submittedQuery && (
         <div className="source-display">
           <div className="circle orange-circle">
             <img src={image} alt="Source Icon" className="source-image" />
@@ -75,23 +68,29 @@ const ChatArea = ({ query, setQuery, handleQuerySubmit, loading, shabadDetails, 
         </div>
       )}
 
-      {/* Display Results */}
       <div className="results-section">
         {loading && <p>Loading...</p>}
         {!loading && shabadDetails.length === 0 && <p>No results found. Try another query.</p>}
         {!loading && shabadDetails.length > 0 && (
           <>
-            <div className="result-card top-result">
-              <h3 className="gurbani-title orange-text">TOP RESULT </h3>
-              {shabadDetails[0].verses.map((verse, index) => (
-                <div key={index}>
-                  <p className="gurbani-verse">{verse.verse}</p>
-                  <p className="gurbani-translation">{verse.translation}</p>
+            {!isExpanded ? (
+              <div className="result-card top-result">
+                <h3 className="gurbani-title orange-text">TOP RESULT</h3>
+                <div className="expand-icon-container" onClick={toggleExpand}>
+                  <img src={expandicon} alt="Expand" className="expand-icon-img" />
                 </div>
-              ))}
-            </div>
-            {/* Display Other Results */}
-            {shabadDetails.slice(1).map((shabad) => (
+                {shabadDetails[0].verses.map((verse, index) => (
+                  <div key={index}>
+                    <p className="gurbani-verse">{verse.verse}</p>
+                    <p className="gurbani-translation">{verse.translation}</p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <ExpandedShabad shabad={shabadDetails[0]} onClose={toggleExpand} />
+            )}
+
+            {!isExpanded && shabadDetails.slice(1).map((shabad) => (
               <div key={shabad.shabadId} className="result-card">
                 <h3 className="gurbani-title orange-text">OTHER RESULTS</h3>
                 {shabad.verses.map((verse, index) => (
@@ -106,8 +105,7 @@ const ChatArea = ({ query, setQuery, handleQuerySubmit, loading, shabadDetails, 
         )}
       </div>
 
-      {/* Suggestions for Gurbani search, only show if the user has typed something */}
-      {showSuggestions && (
+      {!isExpanded && showSuggestions && (
         <div className="suggestions-container">
           {suggestions.map((suggestion, index) => (
             <div key={index} className="suggestion-box" onClick={() => setQuery(suggestion)}>
@@ -117,20 +115,21 @@ const ChatArea = ({ query, setQuery, handleQuerySubmit, loading, shabadDetails, 
         </div>
       )}
 
-      {/* Input area at the bottom */}
-      <form className="query-form" onSubmit={handleFormSubmit}>
-        <p className="query-instruction">Type your question above</p>
-        <input
-          className="query-input"
-          type="text"
-          placeholder="Enter your query..."
-          value={query}
-          onChange={handleInputChange} // Update the input change handler
-        />
-        <button className="query-submit-btn" type="submit">
-          <span className="arrow">&#x2794;</span>
-        </button>
-      </form>
+      {!isExpanded && (
+        <form className="query-form" onSubmit={handleFormSubmit}>
+          <p className="query-instruction">Type your question above</p>
+          <input
+            className="query-input"
+            type="text"
+            placeholder="Enter your query..."
+            value={query}
+            onChange={handleInputChange}
+          />
+          <button className="query-submit-btn" type="submit">
+            <span className="arrow">&#x2794;</span>
+          </button>
+        </form>
+      )}
     </div>
   );
 };
